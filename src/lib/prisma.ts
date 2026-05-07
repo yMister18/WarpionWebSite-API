de49e4997545154454d845ae686f20f6d8c5001a
yMister18 <22nunogaspar@gmail.com>
2026-05-07 15:31:33 +0100

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,8 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // Removi log 'warn' para limpar o log de build, deixe apenas se precisar depurar
-    log: ['error'], 
+    log: ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
