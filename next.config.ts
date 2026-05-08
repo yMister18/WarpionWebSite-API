0c6cb5dcbb47620cadcc73387cb284be49e71b88
yMister18 <22nunogaspar@gmail.com>
2026-05-08 09:38:29 +0100

    fix: set prisma engineType to library and fix next.config serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Tente esta abordagem alternativa se a anterior falhou
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
-  // Mantenha esta também
   serverExternalPackages: ['@prisma/client'],
 };
 
